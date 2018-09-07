1a14c74aa83709d225bcd395faf90a503b8a921c
ABBs0luteZer0 <41222718+ABBs0luteZer0@users.noreply.github.com>
2018-09-07 17:29:32 +0900

    New UI & JS
    1. Added player leaderboard
    2. Changed UI
    3. Added data source as JSON array in js file

---
+++ static/js/raid-data.js
@@ -0,0 +1,18 @@
+var raidDataObj = 
+ '['
++'{"raidBoss":"248","ongoing":"true","level":"4","solo":"","duo":"61","trio":"","quadro":""},'
++'{"raidBoss":"112","ongoing":"true","level":"4","solo":"","duo":"23","trio":"","quadro":""},'
++'{"raidBoss":"359","ongoing":"true","level":"4","solo":"","duo":"85","trio":"","quadro":""},'
++'{"raidBoss":"068","ongoing":"true","level":"3","solo":"81","duo":"","trio":"","quadro":""},'
++'{"raidBoss":"221","ongoing":"true","level":"3","solo":"88","duo":"","trio":"","quadro":""},'
++'{"raidBoss":"249","ongoing":"false","level":"5","solo":"","duo":"","trio":"","quadro":"20"},'
++'{"raidBoss":"379","ongoing":"false","level":"5","solo":"","duo":"","trio":"39","quadro":""},'
++'{"raidBoss":"378","ongoing":"false","level":"5","solo":"","duo":"","trio":"","quadro":"45"},'
++'{"raidBoss":"076","ongoing":"false","level":"4","solo":"","duo":"47","trio":"","quadro":""},'
++'{"raidBoss":"142","ongoing":"false","level":"3","solo":"60","duo":"","trio":"","quadro":""},'
++'{"raidBoss":"095","ongoing":"false","level":"3","solo":"37","duo":"","trio":"","quadro":""},'
++'{"raidBoss":"344","ongoing":"false","level":"3","solo":"10","duo":"","trio":"","quadro":""},'
++'{"raidBoss":"134","ongoing":"false","level":"3","solo":"50","duo":"","trio":"","quadro":""}'
++']'
+;
+
---
+++ static/js/raid-leaderboard.js
@@ -0,0 +1,56 @@
+function populateRaidData(){
+	var myObj, txt="";
+
+	myObj = JSON.parse(raidDataObj);
+
+	myObj.sort(function (a, b) {
+		if(a.ongoing==b.ongoing){
+			if(a.level==b.level){
+				return a.raidBoss.localeCompare(b.raidBoss);
+			}else{
+				return b.level.localeCompare(a.level);
+			}
+		}else{
+			return b.ongoing.localeCompare(a.ongoing);
+		}
+	});
+
+	for (x in myObj) {
+		txt += "<tr><td><img src='https://serebii.net/pokemongo/pokemon/"
+			+myObj[x].raidBoss
+			+".png'/><br/><h6>";
+		var y;
+		for(y=0; y<myObj[x].level; y++){
+			txt += "⭐";
+		}
+		txt += "</h6></td>";
+
+		if(myObj[x].solo!=""){
+			txt += "<td><span class='rounded-circle solo'>"
+				+ myObj[x].solo +"</span></td>";
+		}else{
+			txt += "<td></td>";
+		}
+		if(myObj[x].duo!=""){
+			txt += "<td><span class='rounded-circle duo'>"
+				+ myObj[x].duo +"</span></td>";
+		}else{
+			txt += "<td></td>";
+		}
+		if(myObj[x].trio!=""){
+			txt += "<td><span class='rounded-circle trio'>"
+				+ myObj[x].trio +"</span></td>";
+		}else{
+			txt += "<td></td>";
+		}
+		if(myObj[x].quadro!=""){
+			txt += "<td><span class='rounded-circle quadro'>"
+			+ myObj[x].quadro +"</span></td>";
+		}else{
+			txt += "<td></td>";
+		}
+		
+		txt += "<td></td></tr>";
+	}
+	document.getElementById("rLeaderTable").innerHTML = txt;
+}
